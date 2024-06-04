8d9d9b55c156765d0f7f3bb1344640c1c5bd2769
Zero <john.royer@gmail.com>
2024-06-04 12:24:48 +0800

    save phrase

--- go/word-count/word_count.go
+++ go/word-count/word_count.go
@@ -3,18 +3,27 @@ package wordcount
 type Frequency map[string]int
 
 func WordCount(phrase string) Frequency {
+	freq := make(Frequency)
 	strLength := len(phrase)
 	index := 0
-	tmpString := make([]string, 0)
 	lastSplitWordIndex := 0
 	from := 0
 	length := 0
+	var tmpString string
 
 	for {
 		if isSaperateWord(phrase[0:1]) {
 			from = lastSplitWordIndex + 1
 			length = index - lastSplitWordIndex
-			tmpString = append(tmpString, phrase[from:length])
+			tmpString = phrase[from:length]
+
+			counter, ok := freq[tmpString]
+			if ok {
+				freq[tmpString] = counter + 1
+			} else {
+				freq[tmpString] = 1
+			}
+
 		} else {
 			index++
 		}
