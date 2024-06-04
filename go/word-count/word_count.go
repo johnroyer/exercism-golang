5404c4e5869a7b2c577079e089dffe0e60ab3e5e
Zero <john.royer@gmail.com>
2024-06-04 12:03:01 +0800

    add custom function

--- go/word-count/word_count.go
+++ go/word-count/word_count.go
@@ -3,5 +3,26 @@ package wordcount
 type Frequency map[string]int
 
 func WordCount(phrase string) Frequency {
-	panic("Please implement the WordCount function")
+	strLength := len(phrase)
+	index := 0
+	tmpString := make([]string, 0)
+
+	for {
+		if index == strLength {
+			break
+		}
+	}
+}
+
+func isSaperateWord(word string) bool {
+	switch word {
+	case ":":
+	case "!":
+	case "?":
+	case "\t":
+	case "\n":
+	case " ":
+		return true
+	}
+	return false
 }
