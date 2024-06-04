e30e401fa937702d5383ff7d0f56f7b11bda54be
Zero <john.royer@gmail.com>
2024-06-04 12:14:06 +0800

    implement character checking

--- go/word-count/word_count.go
+++ go/word-count/word_count.go
@@ -6,8 +6,19 @@ func WordCount(phrase string) Frequency {
 	strLength := len(phrase)
 	index := 0
 	tmpString := make([]string, 0)
+	lastSplitWordIndex := 0
+	from := 0
+	length := 0
 
 	for {
+		if isSaperateWord(phrase[0:1]) {
+			from = lastSplitWordIndex + 1
+			length = index - lastSplitWordIndex
+			tmpString = append(tmpString, phrase[from:length)
+		} else {
+			index++
+		}
+
 		if index == strLength {
 			break
 		}
