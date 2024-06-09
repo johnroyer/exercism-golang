06d7f0d6975470ca6c2f3e1840bb9557527b169b
Zero <john.royer@gmail.com>
2024-06-09 13:44:24 +0800

    Fix counting error

--- go/cars-assemble/cars_assemble.go
+++ go/cars-assemble/cars_assemble.go
@@ -11,7 +11,7 @@ func CalculateWorkingCarsPerHour(productionRate int, successRate float64) float6
 // CalculateWorkingCarsPerMinute calculates how many working cars are
 // produced by the assembly line every minute.
 func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) int {
-	return int(float64(productionRate) * (successRate / 100))
+	return int(float64(productionRate) / 60 * (successRate / 100))
 }
 
 // CalculateCost works out the cost of producing the given number of cars.
