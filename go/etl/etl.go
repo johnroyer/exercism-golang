a7089635c1fdb193b58298c9773202beb0dff7b0
Zero <john.royer@gmail.com>
2024-06-09 14:03:08 +0800

    implement converting

--- go/etl/etl.go
+++ go/etl/etl.go
@@ -1,7 +1,17 @@
 package etl
 
+import (
+	"strings"
+)
+
 func Transform(in map[int][]string) map[string]int {
 	scoreMap := make(map[string]int)
+
+	for score, chars := range in {
+		for _, char := range chars {
+			scoreMap[strings.ToLower(char)] = score
+		}
+	}
 
 	return scoreMap
 }
